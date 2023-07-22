34d3e8359d87ee1fa5b234302502ffce25ebc456
NotosWind <windnotos@gmail.com>
2023-07-23 01:26:21 +0300

    Update_1.1

--- Moscow CCG/Assets/CCG/CardEffect.cs
+++ Moscow CCG/Assets/CCG/CardEffect.cs
@@ -1,11 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Effect", menuName = "Card Effect")]
+[CreateAssetMenu(fileName = "New CardEffect", menuName = "Card Effect")]
 public class CardEffect : ScriptableObject
 {
     public string effectName;
     public string effectDescription;
     public int attackModifier;
     public int healthModifier;
-    public string description;
+
+    public void ApplyEffect(Card card)
+    {
+        card.attack += attackModifier;
+        card.health += healthModifier;
+    }
 }
