e3062fce9affd868298fe2d50285695bcab159e9
NotosWind <windnotos@gmail.com>
2023-07-23 01:31:32 +0300

    Update Deck.cs

--- Moscow CCG/Assets/CCG/Deck.cs
+++ Moscow CCG/Assets/CCG/Deck.cs
@@ -1,89 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Deck : MonoBehaviour
 {
-    [System.Serializable]
-    public class CardData
-    {
-        public string cardName;
-        public int attack;
-        public int health;
-        public int quantity;
-    }    
-    public List<CardData> cardDataList = new List<CardData>();
-    public GameObject cardPrefab;
-    public Button drawButton;
-
-    private List<GameObject> cards = new List<GameObject>();
-
-    private void Start()
-    {
-        drawButton.onClick.AddListener(DrawCardAndAddToHand);
-        CreateDeck();
-        Shuffle();
-    }
-
-    private void CreateDeck()
-    {
-        foreach (CardData cardData in cardDataList)
-        {
-            for (int i = 0; i < cardData.quantity; i++)
-            {
-                CreateCard(cardData);
-            }
-        }
-    }
-
-    private void CreateCard(CardData cardData)
-    {
-        GameObject cardGO = Instantiate(cardPrefab, transform);
-        CardDisplay cardDisplay = cardGO.GetComponent<CardDisplay>();
-        cardDisplay.card = new Card();
-        cardDisplay.card.Init(cardDisplay);
-        cardDisplay.card.attack = cardData.attack;
-        cardDisplay.card.health = cardData.health;
-        cardDisplay.UpdateCardInfo();
-        AddCard(cardGO);
-    }
-
-    public void AddCard(GameObject card)
-    {
-        cards.Add(card);
-    }
+    public List<Card> cards;
 
     public void Shuffle()
     {
-        for (int i = cards.Count - 1; i > 0; i--)
+        for (int i = 0; i < cards.Count; i++)
         {
-            int randomIndex = Random.Range(0, i + 1);
-            GameObject temp = cards[i];
+            Card temp = cards[i];
+            int randomIndex = Random.Range(i, cards.Count);
             cards[i] = cards[randomIndex];
             cards[randomIndex] = temp;
-        }
-    }
-
-    public GameObject DrawCard()
-    {
-        if (cards.Count == 0)
-        {
-            Debug.LogError("The deck is empty!");
-            return null;
-        }
-
-        GameObject drawnCard = cards[0];
-        cards.RemoveAt(0);
-        return drawnCard;
-    }
-
-    public void DrawCardAndAddToHand()
-    {
-        GameObject drawnCard = DrawCard();
-        if (drawnCard != null)
-        {
-            FindObjectOfType<Hand>().AddCardToHand(drawnCard);
         }
     }
 }
