841794e258781315f67784885d39abb4e83b2807
NotosWind <windnotos@gmail.com>
2023-07-23 01:32:45 +0300

    Update Hand.cs

--- Moscow CCG/Assets/CCG/Hand.cs
+++ Moscow CCG/Assets/CCG/Hand.cs
@@ -1,24 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Hand : MonoBehaviour
 {
-    public Transform handZone;
+    public List<Card> cards;
 
-    public void AddCardToHand(GameObject card)
+    public void AddCard(Card card)
     {
-        Deck deck = FindObjectOfType<Deck>();
-        Button shuffleButton = GameObject.Find("ShuffleButton").GetComponent<Button>();
-        Button drawButton = GameObject.Find("DrawButton").GetComponent<Button>();
+        cards.Add(card);
+    }
 
-        Vector3 originalScale = card.transform.localScale; // Save the original scale
-        GameObject newCard = Instantiate(card, handZone);
-        newCard.GetComponent<CardDisplay>().deck = deck;
-        // newCard.GetComponent<CardDisplay>().hand = hand;
-        newCard.GetComponent<CardDisplay>().shuffleButton = shuffleButton;
-        newCard.GetComponent<CardDisplay>().drawButton = drawButton;
-        newCard.transform.localScale = originalScale; // Set the scale of the instantiated card to the original scale
+    public void RemoveCard(Card card)
+    {
+        cards.Remove(card);
     }
 }
