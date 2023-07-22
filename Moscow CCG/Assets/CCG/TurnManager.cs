ecff72d3e5f4bbfe0938895424274584bc81083c
NotosWind <windnotos@gmail.com>
2023-07-23 01:34:11 +0300

    Update TurnManager.cs

--- Moscow CCG/Assets/CCG/TurnManager.cs
+++ Moscow CCG/Assets/CCG/TurnManager.cs
@@ -4,46 +4,10 @@ using UnityEngine;
 
 public class TurnManager : MonoBehaviour
 {
-    public enum GameState
-    {
-        Player1Turn,
-        Player2Turn,
-        GameOver
-    }
-
-    public GameState currentState;
-
-    private void Start()
-    {
-        currentState = GameState.Player1Turn;
-    }
+    public bool isPlayerTurn;
 
     public void EndTurn()
     {
-        if (currentState == GameState.Player1Turn)
-        {
-            currentState = GameState.Player2Turn;
-        }
-        else if (currentState == GameState.Player2Turn)
-        {
-            currentState = GameState.Player1Turn;
-        }
-    }
-
-    private void Update()
-    {
-        if (currentState == GameState.GameOver)
-        {
-            return;
-        }
-
-        if (currentState == GameState.Player1Turn)
-        {
-            // Handle player 1's turn
-        }
-        else if (currentState == GameState.Player2Turn)
-        {
-            // Handle player 2's turn
-        }
+        isPlayerTurn = !isPlayerTurn;
     }
 }
